01d2e04aa52bd43a9b16d9939e562958ea3e490d
Daniel Dąbrowski <elosik123@onet.pl>
2018-05-19 23:46:53 +0200

    Probably final version

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 (function () {
 	const addNote = document.getElementById('add'),
-		  app = document.getElementById('app');
+		app = document.getElementById('app');
 	let offsetX, offsetY, currentItem;
-	
+
 	//adding notes
-	addNote.addEventListener('click', function() {
+	addNote.addEventListener('click', createNote);
+	function createNote(left, top, text) {
 		let node = document.createElement('div'),
 			textarea = document.createElement('textarea'),
 			bar = document.createElement('div'),
@@ -20,39 +21,54 @@
 		bar.appendChild(remove);
 		node.appendChild(bar);
 		node.appendChild(textarea);
+		if(left) {
+			node.style.left = left;
+			node.style.top = top;
+			textarea.textContent = text;
+		}
 		app.appendChild(node);
 		node.addEventListener('mousedown', makeDraggable);
-	});
+		save();
+	}
 	
 	//draggable
 	function makeDraggable(e) {
 		currentItem = this;
 		makeFirst(currentItem);
-		if(e.target.nodeName === 'DIV') {
+		if (e.target.nodeName === 'DIV') {
 			offsetX = e.clientX - parseInt(this.offsetLeft);
 			offsetY = e.clientY - parseInt(this.offsetTop);
 			window.addEventListener('selectstart', disableSelect);
 			window.addEventListener('mousemove', positionElement);
-			window.addEventListener('mouseup', function() {
+			window.addEventListener('mouseup', function () {
 				window.removeEventListener('mousemove', positionElement);
 				window.removeEventListener('selectstart', disableSelect);
 			});
+		} else if (e.target.nodeName === 'TEXTAREA') {
+			window.addEventListener('keyup', function() {
+				save();
+			})
 		}
 	}
+	
+	//disable select while dragging
 	function disableSelect(e) {
 		e.preventDefault();
 	}
 	
 	function positionElement(e) {
-		currentItem.style.top = e.clientY - offsetY +"px";
-		currentItem.style.left = e.clientX - offsetX +"px";
+		currentItem.style.top = e.clientY - offsetY + "px";
+		currentItem.style.left = e.clientX - offsetX + "px";
+		save();
 	}
-	
+
 	//remove
 	function removeNote() {
 		this.parentNode.parentNode.remove();
+		save();
 	}
 	
+	//make on top
 	function makeFirst(item) {
 		let elements = document.querySelectorAll('.note');
 		for (let i = 0; i < elements.length; i++) {
@@ -60,4 +76,29 @@
 		}
 		item.classList.add('index');
 	}
+
+	//save
+	function save() {
+		let notes = [];
+		for (let i = 1; i < app.children.length; i++) {
+			notes.push({
+				id: i,
+				left: app.children[i].style.left,
+				top: app.children[i].style.top,
+				text: app.children[i].children[1].value
+			});
+		}
+		localStorage.setItem('notes', JSON.stringify(notes));
+	}
+	
+	//load
+	function load() {
+		let notesLoad = JSON.parse(localStorage.getItem('notes'));
+		if (notesLoad != null && notesLoad != undefined) {
+			for (let i = 0; i < notesLoad.length; i++) {
+				createNote(notesLoad[i].left, notesLoad[i].top, notesLoad[i].text);
+			}
+		}
+	}
+	load();
 })();
